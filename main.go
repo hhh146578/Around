0b4d48968254f4e56d80ee0fd7b9a4dbc70f7dea
Wen <hhh146578>
2018-06-09 12:21:23 -0400

    update ip address

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.192.185.97:9200"
+	ES_URL = "http://35.232.63.32:9200"
 )	
 
 func main() {
